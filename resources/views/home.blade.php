0a9f113e2c424fea00f290fe9d0413f77e1cb394
VlastimilVasut <vasutvlastimil@gmail.com>
2024-07-04 16:30:10 +0200

    You can now scroll thourgh the posts in home but I need to make it load more posts each time you scroll

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connectify / Home</title>
+    <style>
+        body 
+        {
+            overflow: hidden;
+        }
+        .scroll {
+            height: 50%; /* Adjust the height as needed */
+            overflow-y: auto;
+        }
+    </style>
 </head>
 <body>
     <x-app username="{{$_SESSION['username']}}">
-       <div class="container-fluid">
-            <x-HomePost loggedusername="{{$_SESSION['username']}}"></x-HomePost>
+       <div class="container-fluid border">
+            <div class="scroll">
+                <x-HomePost loggedusername="{{$_SESSION['username']}}"></x-HomePost><br>
+                <x-HomePost loggedusername="{{$_SESSION['username']}}"></x-HomePost><br>
+                <x-HomePost loggedusername="{{$_SESSION['username']}}"></x-HomePost><br>
+                <x-HomePost loggedusername="{{$_SESSION['username']}}"></x-HomePost><br>
+                <x-HomePost loggedusername="{{$_SESSION['username']}}"></x-HomePost><br>
+                <x-HomePost loggedusername="{{$_SESSION['username']}}"></x-HomePost><br>
+                <x-HomePost loggedusername="{{$_SESSION['username']}}"></x-HomePost><br>
+            </div>
         </div> 
     </x-app>
     
