c0968351451ea74d1ad5446e625b5f6b2542ac51
VlastimilVasut <vasutvlastimil@gmail.com>
2024-07-18 15:00:57 +0200

    "Updated Post.blade.php, removed postMenu.blade.php, and modified home.blade.php and profile.blade.php to reflect changes in post component and styling."

--- resources/views/components/Post.blade.php
+++ resources/views/components/Post.blade.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/post.css')}}">
+    <script src="{{asset('js/Post.js')}}"></script>
 </head>
 <body>
     <?php 
@@ -49,7 +50,7 @@
                         <div class="col-3 fs-6 d-flex justify-content-center align-content-center border">
                             {{ $post['PostDate'] }}
                         </div>
-                        <div class="col-3 d-flex justify-content-center align-content-center border">
+                        <div class="col-3 d-flex justify-content-center align-items-center border">
                             <div class="dropdown">
                                 <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Menu
@@ -72,6 +73,88 @@
                 }
             }
         }
+        elseif(request()->is('home') || request()->is('/'))
+        {
+            $posts = DbHandlerController::queryAll('SELECT * FROM Posts ORDER BY RAND() LIMIT 1');
+            foreach($posts as $post)
+            {
+                $postID = $post['Post_ID'];
+                $ID = $post['ID'];
+                $content = $post['Content'];
+                $postDate = $post['PostDate'];
+                $users = DbHandlerController::queryAll('SELECT * FROM Users WHERE ID=?', $ID);
+                foreach($users as $user)
+                {
+                    $name = $user['Name'];
+                    $username = $user['Username'];
+                    $likes = DbHandlerController::queryAll('SELECT * FROM IsLiked WHERE ID=? AND Post_ID=?', $ID, $postID);
+                    $IsLiked = 0;
+                    foreach($likes as $like)
+                    {
+                        $IsLiked = $like['IsLiked'];
+                    }
+                    $style = "";
+                    if($IsLiked == 0) $style = "post-item bi bi-heart";
+                    else  $style = "post-item bi bi-heart-fill";
+                    ?>
+                        <div class="row mt-2">
+                            <div class="col-2 border d-flex align-items-center justify-content-end">
+                                <img src="{{asset("images/DefaultPFP.png")}}" alt="pfp" class="pfp-50 img-fluid rounded-circle">
+                            </div>
+                            <div class="col-3 border">
+                                <div class="fs-5">
+                                    {{$name}}
+                                </div>
+                                <div class="text-muted fs-6">
+                                    {{"@$username"}}
+                                </div>
+                            </div>
+                            <div class="col-3 d-flex align-items-center justify-content-center border">
+                                {{$postDate}}
+                            </div>
+                            <div class="col-4 d-flex justify-content-center align-items-center border">
+                                <div class="dropdown">
+                                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        Menu
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        @if($username!= $_SESSION['username'])
+                                            <li><a class="dropdown-item" href="/report/{{ $postID }}">Report</a></li>
+                                        @elseif($username  == $_SESSION['username'])
+                                            <li><a class="dropdown-item" href="/deletePost/{{ $postID }}">Delete</a></li>
+                                        @endif
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 text-center border">
+                                {{$content}}
+                            </div>
+                        </div>
+                        <x-postOpts userID="{{$ID}}" postID="{{$postID}}" heartStyle="{{$style}}"></x-postOpts>
+                    <?php
+                }
+            }
+        }
         ?>
+<script type="text/javascript">
+    $(document).ready(() => {
+        $("#postMenuForm").on('submit', (e) => {
+            e.preventDefault();
+            $.ajax({
+                url: "/ajaxDeletePostUser",
+                type: "POST",
+                data: $("#postMenuForm").serialize(),
+                success: (resp) => {
+                    location.reload();
+                },
+                error: (xhr, status, error) => {
+                        console.error(xhr.responseText);
+                }
+            });
+        });
+    });
+</script>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connectify / Home</title>
     <link rel="stylesheet" href="{{asset('css/home.css')}}">
+    <link rel="stylesheet" href="{{asset('css/pfp.css')}}">
+    <link rel="stylesheet" href="{{asset('css/scroll.css')}}">
 </head>
 <body>
     <x-app username="{{$_SESSION['username']}}">
        <div class="container-fluid border">
             <div class="scroll" id="Posts">
-                
+                <x-Post profileUsername=""></x-Post>
             </div>
         </div> 
     </x-app>
